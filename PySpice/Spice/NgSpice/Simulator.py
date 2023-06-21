38acadc9c94e28b9e8a9d1d5ae4104896e51d2dc
Marcus Mellor <infinitymdm@gmail.com>
2023-06-21 12:08:22 -0500

    Pass measurements through to Analysis object

--- PySpice/Spice/NgSpice/Simulator.py
+++ PySpice/Spice/NgSpice/Simulator.py
@@ -115,11 +115,11 @@ class NgSpiceSharedSimulator(NgSpiceSimulator):
         # load circuit and simulation
         # Fixme: Error: circuit not parsed.
         self._ngspice_shared.load_circuit(str(simulation))
-        self._ngspice_shared.run()
+        measurements = self._ngspice_shared.run()
         self._logger.debug(str(self._ngspice_shared.plot_names))
 
         plot_name = self._ngspice_shared.last_plot
         if plot_name == 'const':
             raise NameError('Simulation failed')
 
-        return self._ngspice_shared.plot(simulation, plot_name).to_analysis()
+        return self._ngspice_shared.plot(simulation, plot_name).to_analysis(measurements)
