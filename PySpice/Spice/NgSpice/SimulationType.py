1dda0fb654c4a88bc523c2d462aa46998d9efab3
Marcus Mellor <infinitymdm@gmail.com>
2025-01-13 10:58:01 -0600

    Update NgSpice version to 43

--- PySpice/Spice/NgSpice/SimulationType.py
+++ PySpice/Spice/NgSpice/SimulationType.py
@@ -82,7 +82,7 @@ SIMULATION_TYPE[27] = (
     'charge',
 )
 
-LAST_VERSION = 42   # released on 2023-12-27
+LAST_VERSION = 43   # released on July 14th, 2024
 
 for version in range(28, LAST_VERSION +1):
     SIMULATION_TYPE[version] = SIMULATION_TYPE[27]
